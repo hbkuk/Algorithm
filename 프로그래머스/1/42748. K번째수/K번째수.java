0542a8037bb3874ad007a399a1cc9e78408814b4
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2023-11-15 12:13:27 +0900

    [level 1] Title: K번째수, Time: 2.20 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        // 결과 배열
         List<Integer> result = new ArrayList<>();
-
-        for (int[] command : commands) {
+        
+        for(int[] command : commands) {
             int i = command[0];
             int j = command[1];
             int k = command[2];
-
-            // 배열 자르기
+            
             int[] tempArray = Arrays.copyOfRange(array, i - 1, j);
             
-            // 정렬
             Arrays.sort(tempArray);
-
-            // k번째 수 찾기
+            
             result.add(tempArray[k - 1]);
         }
-
-        // List를 int[] 배열로 변환
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        
+        // List를 int[] 변환
+        return result.stream()
+                    .mapToInt(Integer::intValue).toArray();
     }
 }
