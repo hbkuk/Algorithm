dfdb1f869140a6ca80a7ff18fea6f9a1f26f76a9
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2023-11-16 13:25:23 +0900

    [Silver IV] Title: 스택, Time: 212 ms, Memory: 20232 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,23 +1,23 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
+    
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int testCount = Integer.parseInt(br.readLine());
 
-        // 1. Stack 구현하기
         int size = 5;
         int lastIndex = 0;
         int[] array = new int[size];
-
-        String[] data = new String[2];
-        // 2. 문자열 "push 1"을 전달받으면 어떻게 분리할 것인지...
+        
         for (int i = 0; i < testCount; i++) {
-            data = br.readLine().split(" ");
+            String[] data = br.readLine().split(" ");
             
             if(data[0].equals("push")) {
                 if(array.length == lastIndex + 1) {
@@ -38,36 +38,39 @@ public class Main {
 
             if(data[0].equals("pop")) {
                 if(lastIndex != 0) {
-                    System.out.println(array[lastIndex - 1]);
+                    sb.append(array[lastIndex - 1]).append('\n');
                     lastIndex--;
                 } else {
-                    System.out.println(-1);
+                    sb.append(-1).append('\n');
                 }
                 continue;
             }
             
             if(data[0].equals("top")) {
                 if(lastIndex != 0) {
-                    System.out.println(array[lastIndex - 1]);
+                    sb.append(array[lastIndex - 1]).append('\n');
                 } else {
-                    System.out.println(-1);
+                    sb.append(-1).append('\n');
                 }
                 continue;
             }
 
             if(data[0].equals("size")) {
-                System.out.println(lastIndex);
+                sb.append(lastIndex).append('\n');
                 continue;
             }
 
             if(data[0].equals("empty")) {
                 if(lastIndex == 0) {
-                    System.out.println(1);
+                    sb.append(1).append('\n');
                 } else {
-                    System.out.println(0);
+                    sb.append(0).append('\n');
                 }
                 continue;
             }
         }
+        System.out.println(sb);
+
+        br.close();
     }
 }
