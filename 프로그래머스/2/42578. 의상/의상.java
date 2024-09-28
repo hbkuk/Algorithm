805e139ad424a93512fac829a8c0004b907a85ca
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-09-28 12:55:17 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,33 +1,34 @@
-// 문제 분석)
-// 매일 다른 옷을 입어야 할 때, 조합의 수 return
-// 종류별 1개의 옷만 가능
-// 종류가 일부 겹치더라도, 다른 의상이 겹치지 않으면 + 1
-// 종류가 일부 겹치더라도, 의상을 추가로 착용하면 + 1
-// 하루에 최소 한 개 이상은 입어야 함.
-
-// 문제 최종목표 
-// 종류 별 숫자가 있을 때, 순서를 고려해서 최소 1개를 선택해서 조합한다면?
-
-// 문제 접근방식
-// 1. 종류를 구분한다.
-//  - 얼굴, 상의, 하위 등..
-// 2. 그렇다면, 최소 1개 선택을 하고 종류 수까지 선택하는 조합의 수를 구한다.
-//  - 
 import java.util.*;
+
 class Solution {
-    public int solution(String[][] clothes) {
     
-        // 1. clothes를 순회하면서, Map에 담는다.
-        Map<String, Integer> clothesMap = new HashMap<>();
-        for(String[] cloth : clothes)  {
-            clothesMap.put(cloth[1], clothesMap.getOrDefault(cloth[1], 0) + 1);
+    // 하루에 최소 한 개의 의상
+    // 다른 의상이 겹치지 않거나, 의상을 추가로 착용한 경우는 다른 방법으로 간주
+    public int solution(String[][] clothes) {
+        // TODO : 최소 1개 이상의, 서로 다른 조합의 수는?
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for(String[] cloth : clothes) {
+            String product = cloth[0];
+            String category = cloth[1];
+                
+            List<String> list = map.get(category);
+            // 1) map에 존재하지 않다면 -> List 생성 후 넣기
+            if(list == null) {
+                List<String> productList = new ArrayList<>();
+                productList.add(product);
+                map.put(category, productList);   
+            // 2) map에 존재한다면 -> List에 추가하기
+            } else {
+                list.add(product);
+            }
         }
         
         int combinationCount = 1;
-        for (int count : clothesMap.values()) {
-            combinationCount *= (count + 1); // 해당 종류의 의상을 선택하지 않는 경우까지 포함하여 곱셈
+        for(String category : map.keySet()){
+            combinationCount *= map.get(category).size() + 1;
         }
-            
+        
         return combinationCount - 1;
     }
 }
