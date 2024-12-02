6bee7c0513003fb8673a792163bb39ec6cfd1d28
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2024-12-02 23:54:47 +0900

    [Silver III] Title: 타노스, Time: 180 ms, Memory: 17712 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -4,25 +4,49 @@ public class Main {
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         String line = scan.nextLine(); // 0과 1로 이루어진 문자열
-        
         int zeroCount = 0;
         int oneCount = 0;
-        for(char c : line.toCharArray()) {
-            if(c == '0') {
-                zeroCount ++;
+
+        // 1. 0과 1의 개수 계산
+        for (char c : line.toCharArray()) {
+            if (c == '0') {
+                zeroCount++;
             } else {
-                oneCount ++;
+                oneCount++;
             }
         }
-        
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append("0".repeat(Math.max(0, zeroCount / 2)));
-        sb.append("1".repeat(Math.max(0, oneCount / 2)));
-        
-        System.out.println(sb);
+
+        int removeZero = zeroCount / 2; // 제거해야 할 0의 개수
+        int removeOne = oneCount / 2;  // 제거해야 할 1의 개수
+
+        StringBuilder result = new StringBuilder();
+        char[] chars = line.toCharArray();
+
+        // 2. 0을 뒤에서부터 제거
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (chars[i] == '0' && removeZero > 0) {
+                chars[i] = 'X'; // 제거된 0 표시
+                removeZero--;
+            }
+        }
+
+        // 3. 1을 앞에서부터 제거
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '1' && removeOne > 0) {
+                chars[i] = 'X'; // 제거된 1 표시
+                removeOne--;
+            }
+        }
+
+        // 4. 결과 문자열 구성
+        for (char c : chars) {
+            if (c != 'X') { // 제거된 문자는 제외
+                result.append(c);
+            }
+        }
+
+        System.out.println(result);
     }
-    
 }
