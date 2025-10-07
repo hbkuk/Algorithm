a6c93a7f8dc944faba8e8eb8bc89f4291e7adb4c
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2025-10-07 09:53:37 +0900

    [level 3] Title: 등굣길, Time: 3.04 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,40 +1,67 @@
+/*
+    오른쪽과 아래쪽으로만 움직여서 이동해보기
+    특정 요소에서는 위, 왼쪽에 숫자에 + 1 씩 해가면서 업데이트 한다.
+**/
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         
-        // 물에 잠긴 지역 표시
+        // 지도 만들기
         boolean[][] map = new boolean[n][m];
-        for (int[] puddle : puddles) {
-            int row = puddle[1] - 1;
-            int col = puddle[0] - 1;
-            map[row][col] = true;
+        for(int i = 0; i < puddles.length; i++) {
+            int[] 물_좌표 = puddles[i];
+            int 물_x좌표 = 물_좌표[0] - 1;
+            int 물_y좌표 = 물_좌표[1] - 1;
+            
+            map[물_y좌표][물_x좌표] = true; // 물에 잠긴 것을 표현
         }
-
-        // DP 배열 생성 및 초기화
-        int[][] dp = new int[n][m];
-        dp[0][0] = 1; // 시작 위치
         
-        // 맵을 순차적을 순회하면서, 도달할 수 있는 경로를 카운팅한다.
+        
+        // dp 갱신하기
+        int[][] dp = new int[n][m];
+        
+        //  - 최초 위치 1로 설정, 단 물에 잠기지 않은 경우
+        for(int i = 0; i < m; i++) {
+            if(map[0][i]) {
+                break;
+            }
+            dp[0][i] = 1;
+        }
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if (i == 0 && j == 0) { // 집인 경우 
-                    dp[i][j] = 1;
+            if(map[i][0]) {
+                break;
+            }
+            dp[i][0] = 1;
+        }
+    
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                
+                int 현재_row = i;
+                int 현재_col = j;
+                
+                // 현재 위치가 물에 잠긴 경우
+                if(map[현재_row][현재_col]) {
+                    dp[현재_row][현재_col] = 0;
                     continue;
-                }
-                if (map[i][j]) { // 웅덩이인 경우
-                    dp[i][j] = 0;
-                    continue;
-                }
-
-                if(i > 0) { // 위에서 온 경우
-                    dp[i][j] = (dp[i][j] + dp[i - 1][j]) % 1_000_000_007;
                 }
                 
-                if(j > 0) { // 좌측에서 온 경우
-                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % 1_000_000_007;
+                int[] dr = {0, -1}; // 왼, 우
+                int[] dc = {-1, 0};
+                
+                
+                int 경로_합산 = 0;
+                // 0. Map을 넘어가진 않았는지
+                for(int k = 0; k < 2; k++) {
+                    int nr = 현재_row + dr[k];
+                    int nc = 현재_col + dc[k];
+                    
+                    경로_합산 += dp[nr][nc];
                 }
+                
+                dp[현재_row][현재_col] = 경로_합산 % 1_000_000_007;
             }
         }
         
-        return dp[n-1][m-1];
+        return dp[n - 1][m - 1];
     }
 }
