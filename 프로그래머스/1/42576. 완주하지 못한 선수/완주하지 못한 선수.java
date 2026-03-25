7a800993562fba3bdad5eaa35691c79dfd5c7f06
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2026-03-25 21:16:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.99 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,31 +1,39 @@
-import java.util.*;
+/**
 
+<문제 내용>
+단 한명의 선수를 제외하고 모든 선수가 마라톤을 완주함.
+완주하지 못한 선수의 이름을 return 하도록 함수 작성.
+
+마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하. (=10^5)
+참가자 중에는 동명이인.
+
+<접근 방식>
+Map<String,Integer> 를 하나 만들어서. 선수들 이름에 해당하는 key에 value를 ++?
+-> 그러면은 value 1인 것을 추출해서 ealry return 하는 것도 괜찮을 듯.
+
+**/
+
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 동명이인 고려 -> 카운팅        
-        Map<String, Integer> participantMap = new HashMap<>();
-        for(int i = 0; i < participant.length; i++) {
-            Integer count = participantMap.get(participant[i]);
-            if(count == null) {
-                participantMap.put(participant[i], 1);
-            } else {
-                participantMap.put(participant[i], count + 1);
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String name : participant) {
+            map.put(name, map.getOrDefault(name, 0) + 1);
+        }
+        
+        for(String name : completion) {
+            map.put(name, map.get(name) - 1);
+        }
+        
+        // value가 1인 것 찾기
+        Set<String> keys = map.keySet();
+        for(String name : keys) {
+            if( map.get(name) == 1) {
+                return name;
             }
         }
         
-        // TODO: compeletion를 순회하면서 카운팅 삭제
-        for(int i =0; i < completion.length; i++) {
-            Integer count = participantMap.get(completion[i]);
-            participantMap.put(completion[i], count - 1);
-        }
-        
-        // TODO: 카운팅 남아있는 것 찾기
-        for(int i = 0; i < participant.length; i++) {
-            Integer count = participantMap.get(participant[i]);
-            if(count != 0) {
-                return participant[i];
-            }
-        }
         return null;
     }
 }
