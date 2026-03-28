54a903a714bbcfcc9e0a32ba4abdca221968bef8
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2026-03-28 19:42:33 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.35 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,60 +1,66 @@
+/**
+
+// 한달을 28일로 기준을 잡고 진행한다.
+// 별도 라이브러리는 사용할 수 없다. -> 모든 날짜를 일수로 계산해서 진행한다.
+
+1. 필요 메서드
+    int toLocalDate (String s) -> 문자열 년/월/일을 받아서 반환한다.
+    int getSaveDate (int day, int plusMonth) -> 현재 날짜와 유효기간 월을 전달받아서 보관 가능한 일자를 반환한다.
+     
+2. 자료 구조
+    문서 최대 보존기간 -> List
+
+3. 구현 계획
+    - 각 privacies를 순회하면서 마지막 보관날짜를 List에 담는다.
+    - 오늘 날짜를 비교하면서 폐기해야할 문서의 인덱스를 담는다.
+    - 반환한다.
+
+**/
+
 import java.util.*;
+import java.time.*;
 
 class Solution {
-    
     public int[] solution(String today, String[] terms, String[] privacies) {
-
-        // [주의 사항]
-        // * 모든 달은 28일까지만 있음.
-        // * 유효기간이 지났다면 반드시 파기
-        // * 보관할 수 있는 달은 1이상 100 이하
-        //  - 연도 계산 필수적...
-        //      - 일을 28 로 나눈 나머지, 일을 28 로 나눈 몫
-        //      - 월을 12 로 나눈 나머지, 월을 12 로 나눈 몫
-        
-        // [풀이 방식]
-        //  1. terms Map을 만들어서 유효기간 관리하기
-        //  2. privacies 와 동일하게 보관 최종일자를 관리하기
-        //  3. 현재 날짜 이후로 보관일자가 지난 번호 추출하기
-        
-        List<Integer> 답 = new ArrayList<>();
-        
-        Map<String, Integer> 유효기간_맵 = new HashMap<>();
-        for(int i = 0; i < terms.length; i++) {
-            String[] 분리된_값 = terms[i].split(" ");
-            
-            유효기간_맵.put(분리된_값[0], Integer.parseInt(분리된_값[1]));
+        Map<String, Integer> termsMap = new HashMap<>();
+        for(String term : terms) {
+            String[] arr = term.split(" ");
+            termsMap.put(arr[0], Integer.parseInt(arr[1]));
         }
         
-        int 현재_날짜_값 = dateToDays(today);
+        List<Integer> list = new ArrayList<>();
+        for(String privacy: privacies) {
+            String[] arr = privacy.split(" ");
+            
+            list.add(getSaveDate(toDay(arr[0]), termsMap.get(arr[1])));
+        }
         
-        for(int i = 0; i < privacies.length; i++) {
-            String[] 분리된_개인정보_수집_정보 = privacies[i].split(" ");
-            
-            String 수집_일자 = 분리된_개인정보_수집_정보[0];
-            String 약관_종류 = 분리된_개인정보_수집_정보[1];
-            
-            // 수집_일자 + 유효기간 => 보관일자
-            int 수집_일자_값 = dateToDays(수집_일자);
-            int 유효기간_값 = 유효기간_맵.get(약관_종류) * 28;
-            int 보관_일자_값 = 수집_일자_값 + 유효기간_값 - 1;
-            
-            if (현재_날짜_값 > 보관_일자_값) {
-                답.add(i + 1);
+        List<Integer> result = new ArrayList<>();
+        
+        int toDay = toDay(today);
+        
+        for(int i = 0; i <= list.size() - 1; i++) {
+            if(toDay > list.get(i)) {
+                result.add(i + 1);
             }
         }
         
-        return 답.stream()
+        return result.stream()
             .mapToInt(Integer::intValue)
-            .toArray(); 
-        }
+            .toArray();
+    }
     
-    int dateToDays(String date) {
-        String[] parts = date.split("\\.");
-        int year = Integer.parseInt(parts[0]);
-        int month = Integer.parseInt(parts[1]);
-        int day = Integer.parseInt(parts[2]);
+    int toDay (String s) {
+        String[] arr = s.split("\\.");
         
-        return year * 12 * 28 + month * 28 + day;
+        int year = Integer.parseInt(arr[0]);
+        int month = Integer.parseInt(arr[1]);
+        int day = Integer.parseInt(arr[2]);
+        
+        return (year * 12 * 28) + (month * 28) + (day);
+    }
+    
+    int getSaveDate (int day, int plusMonth) {
+        return day + (plusMonth * 28) - 1;
     }
 }
