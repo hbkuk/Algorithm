148868d67e67e0061a28bf0a2d5791b62fe227bb
Byeong Kuk <109803585+hbkuk@users.noreply.github.com>
2026-03-28 14:29:48 +0900

    [level 2] Title: H-Index, Time: 4.53 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,38 +1,54 @@
-// H-Index를 나타내는 값인 h 구하기
-// 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용 => h의 최댓 값이 H-Index
+/**
+
+<문제 분석>
+
+* H-Index는?: 논문 n 편 중, h번 이상 인용된 논문이 h편 이상이고, 나머지 논문이 h번 이하 인용되었다면 h의 최댓값
+
+<접근 방식>
+1. 내림차순 정렬
+    - [3, 0, 6, 1, 5]	-> [6, 5, 3, 1, 0]
+2. 가장 큰 숫자부터 확인
+    - 6
+        - (6편 이상 인용된 논문이 6편 이상) && (나머지 논문도 6번 이하 인용)
+    - ...
+    - 최소 0보다 작을 수 없음.
+    
+<성능 확인>
+N * N => O(N) -> 10^6
+
+**/
+
+import java.util.*;
 class Solution {
-    // 최댓값 h를 구하라!!!
-    // 1. 전달받은 배열의 length을 h로 설정한다.
-    // 2. 전달받은 배열에서 h번 이상 인용된 논문이 h편 이상인지 확인한다.
-    //  - h 이상이라면
-    //  => 정답
-    //  - h 미만이라면
-    //  => h -- 한 후, 2번으로 돌아간다.
-    // ===> 최악의 시간복잡도는 10^6 이다. 이정도면 시간초과는 발생하지 않을 것으로 예상한다.
-    // ===> 자! 이제 문제를 풀어보자.
     public int solution(int[] citations) {
-        int h = citations.length;
+        // int[] -> Integer [] (정렬시 필수)
+        Integer[] temp = Arrays.stream(citations)
+            .boxed()
+            .toArray(Integer[]::new);
         
-        for(int i = 0; i < citations.length; i++) {
-            int count = h;
-            // 논문이 h번 이상, h편 이상인지 확인
-            for(int j = 0; j < citations.length; j++) {
-                if(citations[j] >= h) {
-                    count --;
-                }
-                
-                // 혹시 남은 citation을 다 해도, count를 지울 수 없다면 break
-                if(count > citations.length - 1 - j) {
-                    break;
-                }
+        // 내림차순 정렬
+        Arrays.sort(temp, Collections.reverseOrder());
+        
+        // 가장 큰 숫자부터 찾기.
+        int h = temp.length;
+        
+        for(int i = 0; i <= temp.length - 1; i++) {
+            
+            // 기준으로 잡은 h가 길이보다 크다면
+            if(h > temp.length) {
+                h --;
+                continue;
             }
             
-            // 종료 조건
-            if(count <= 0) {
-                break;
+            // 기준과 실제 배열에 있는 값이 동일하다면
+            if(temp[h - 1] >= h) {
+                return h;
+            } else {
+                h --;
+                continue;
             }
-            h --;
         }
-        return h;
+        
+        return 0;
     }
 }
